dd5362781049fb90d0b49399bb4cc40e44cf833e
atlae12 <cemena598@gmail.com>
2026-06-19 21:06:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-import os
 import requests
 import asyncio
 from aiogram import Bot, Dispatcher
 
-# Получаем данные из настроек Railway (Variables)
-BOT_TOKEN = os.getenv('8955345740:AAEt69mAui7qxvXXzQnGkfplIorLc_Qne9k')
-STEAM_API_KEY = os.getenv('27579EE501C27E2A27CC3F63B8CE7884')
-YOUR_CHAT_ID = os.getenv('7764466097')
-# ID друзей через запятую в одной строке в настройках
-FRIENDS_STEAM_IDS = os.getenv('76561199853757393').split(',')
+# ВАШИ КЛЮЧИ (ВСТАВЛЕНЫ В КОД)
+BOT_TOKEN = '8955345740:AAEt69mAui7qxvXXzQnGkfplIorLc_Qne9k'
+STEAM_API_KEY = '27579EE501C27E2A27CC3F63B8CE7884'
+YOUR_CHAT_ID = '7764466097'
+FRIENDS_STEAM_IDS = ['76561199853757393']
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-# Хранилище статусов
 last_statuses = {steam_id: None for steam_id in FRIENDS_STEAM_IDS}
 
 async def check_steam_status():
@@ -30,7 +27,6 @@ async def check_steam_status():
             
             if last_statuses[steam_id] is not None and current_status != last_statuses[steam_id]:
                 name = player.get('personaname', 'Друг')
-                # 0 - оффлайн, 1 - онлайн
                 status_text = "в сети" if current_status != 0 else "вышел из сети"
                 await bot.send_message(chat_id=YOUR_CHAT_ID, text=f"🎮 {name} {status_text}")
             
